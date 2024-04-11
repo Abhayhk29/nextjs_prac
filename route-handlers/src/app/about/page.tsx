35906424e443af47fc6f045853b028afc9d79ed4
abhayhk29 <abhayhk29@gmail.com>
2024-04-11 15:50:47 +0530

    nextjs practice SSR

---
+++ route-handlers/src/app/about/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+import React, { useState } from 'react'
+
+const About = () => {
+    const [data, setdata] = useState('dsdfdsf')
+  return (
+    <div>About {data}</div>
+  )
+}
+
+export default About
